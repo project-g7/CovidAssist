9da521d1aca709644ea935de05a828e91a98d429
Reshani Dilhari <56593064+ReshaniD97@users.noreply.github.com>
2021-07-03 21:55:24 +0530

    new changes

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import {View, Text} from 'react-native';
 
 const App = () => {
   return (
     <View>
       <Text>hello</Text>
+      <Text>CovidAssist</Text>
     </View>
-  )
-}
+  );
+};
 
-export default App
+export default App;
