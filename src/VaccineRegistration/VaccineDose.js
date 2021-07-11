9e96d3b16c400d97a184bf2f925d51179f9df030
LManjitha <lmanjithah@gmail.com>
2021-07-11 18:35:20 +0530

    set dynamic radio buttons

--- src/VaccineRegistration/VaccineDose.js
+++ src/VaccineRegistration/VaccineDose.js
@@ -1,33 +1,74 @@
 import React, {useState} from 'react';
 import RadioGroup from 'react-native-radio-buttons-group';
+import { StyleSheet, View} from 'react-native'
+
 
 const radioButtonsData = [
   {
     id: '1', // acts as primary key, should be unique and non-empty string
     label: 'Yes',
-    value: 'yes',
+    value: true,
     color: '#1167b1',
   },
   {
     id: '2',
     label: 'No',
-    value: 'no',
+    value: false,
+    color: '#1167b1',
+  },
+];
+
+const newRadioButtonsData = [
+  {
+    id: '1', // acts as primary key, should be unique and non-empty string
+    label: 'Dose1',
+    value: 'dose1',
+    color: '#1167b1',
+  },
+  {
+    id: '2',
+    label: 'Dose2',
+    value: 'dose2',
     color: '#1167b1',
   },
 ];
 
 export default function App() {
-  const [radioButtons, setRadioButtons] = useState(radioButtonsData);
+const [radioButtons, setRadioButtons] = useState(radioButtonsData);
+  const [newRadioButtons, setNewRadioButtons] = useState(newRadioButtonsData);
+  const [isYes,setIsYes] = useState(false);
 
   function onPressRadioButton(radioButtonsArray) {
     setRadioButtons(radioButtonsArray);
+    // console.log(radioButtonsArray);
+  }
+  function onPressNewRadioButton(newRadioButtonsArray) {
+    setNewRadioButtons(newRadioButtonsArray);
+    // console.log(radioButtonsArray);
   }
 
+  let selectedButton = radioButtons.find(e => e.selected == true);
+  let newSelectedButton = newRadioButtons.find(e => e.selected == true);
+  selectedButton = selectedButton ? selectedButton.value : console.log("no vals");
+
   return (
-    <RadioGroup
-      radioButtons={radioButtons}
-      onPress={onPressRadioButton}
-      layout="row"
-    />
+        <View>
+            <RadioGroup radioButtons={radioButtons} onPress={onPressRadioButton} layout="row"/>
+            {selectedButton && <View style={styles.box3}>
+              <RadioGroup radioButtons={newRadioButtons} onPress={onPressNewRadioButton} layout="column"/>
+            </View>}
+        </View>
   );
 }
+
+const styles = StyleSheet.create({
+    box3: {
+      marginLeft: -10,
+      marginTop: 25,
+      backgroundColor: 'white',
+      width: 200,
+      height: 80,
+      borderRadius: 8,
+      padding:10
+  },
+})
